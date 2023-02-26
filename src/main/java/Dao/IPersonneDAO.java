fa234a9016a7f020eb13f27005e39df69e424430
Hamza EL MRABTI <elmrabti.hamza@gmail.com>
2023-02-26 20:45:13 +0100

    Modification des dao

--- src/main/java/Dao/IPersonneDAO.java
+++ src/main/java/Dao/IPersonneDAO.java
@@ -6,7 +6,7 @@ import beans.Personne;
 
 public interface IPersonneDAO {
     public void create(Personne p) throws SQLException;
-    public Personne read(int id);
+    public Personne read(int id) throws SQLException;
     public void update(Personne p);
     public void delete(int id);
 }
--- src/main/java/Dao/PersonneDAO.java
+++ src/main/java/Dao/PersonneDAO.java
@@ -2,7 +2,13 @@ package Dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.catalina.ha.backend.CollectedInfo;
 
 import beans.Personne;
 
@@ -27,17 +33,13 @@ public class PersonneDAO implements IPersonneDAO {
 		String insertQuery = "INSERT INTO personne (id, nom, prenom) VALUES (?, ?, ?)";
         PreparedStatement preparedStatement = null;
         
-			preparedStatement = connection.prepareStatement(insertQuery);
+		preparedStatement = connection.prepareStatement(insertQuery);
+		preparedStatement.setInt(1, p.getId() );
+		preparedStatement.setString(2, p.getNom() );
+		preparedStatement.setString(3, p.getPrenom());
 
-			preparedStatement.setInt(1, p.getId() );
-
-			preparedStatement.setString(2, p.getNom() );
-
-			preparedStatement.setString(3, p.getPrenom());
-			
-
-	        System.out.println(preparedStatement);
-			int i = preparedStatement.executeUpdate();
+	    System.out.println(preparedStatement);
+		int i = preparedStatement.executeUpdate();
 			
 	}
 
@@ -47,10 +49,42 @@ public class PersonneDAO implements IPersonneDAO {
 	
 
 	@Override
-	public Personne read(int id) {
+	public Personne read(int id) throws SQLException {
 		// TODO Auto-generated method stub
+		
+		Statement statement = null ;
+		ResultSet resultat = null ;
+		String select ="SELECT * from personne where id= ?;" ;
+		PreparedStatement preparedStatement = connection.prepareStatement(select);
+		preparedStatement.setInt(1, id );
+		
+		if(resultat.next()) {
+			String nom = resultat.getString("nom");
+			String prenom = resultat.getString("prenom") ;
+			return new Personne("nom","prenom");
+		}
 		return null;
 	}
+	
+	
+	public Collection<Personne> read() throws SQLException {
+		// TODO Auto-generated method stub
+		Collection<Personne> personnes = new ArrayList<>() ;
+		Statement statement = null ;
+		ResultSet resultat = null ;
+		String query ="SELECT * from personne" ;
+		resultat = statement.executeQuery(query);
+		
+		if(resultat.next()) {
+			String nom = resultat.getString("nom");
+			String prenom = resultat.getString("prenom") ;
+			personnes.add(new Personne("nom","prenom")) ;
+		}
+		
+		return personnes;
+	}
+	
+	
 
 	@Override
 	public void update(Personne p) {

2058fb18aba9601561f734e07b7b0c4d76525650
Hamza EL MRABTI <elmrabti.hamza@gmail.com>
2023-03-04 21:46:53 +0100

    Resolution probleme de cookies

--- src/main/java/servlet/CookieTest.java
+++ src/main/java/servlet/CookieTest.java
@@ -1,6 +1,5 @@
 package servlet;
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -31,8 +30,18 @@ public class CookieTest extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		String firstName = request.getParameter("Prenom");
+		String lastName = request.getParameter("Nom");
+		System.out.println(firstName);		
+		System.out.println(" ");
+		String title = "title" ;
+		Cookie cookie = new Cookie("username", lastName);  
+		cookie.setMaxAge(24*60*60);
+		response.addCookie(cookie);
+		PrintWriter out = response.getWriter();
+		out.println("<html>\n" +"<head><title>"+ title + "</title></head>\n" + " </body>Hello "+firstName+" "+lastName+"</body></html>"); 
+		
 	}
 
 	/**
@@ -41,13 +50,16 @@ public class CookieTest extends HttpServlet {
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
 		response.setContentType("text/html");
-		String name = request.getParameter("identifiant");
+		String firstName = request.getParameter("Prenom");
+		String lastName = request.getParameter("Nom");
+		System.out.println(firstName);		
+		System.out.println(" ");
 		String title = "title" ;
-		Cookie cookie = new Cookie("username", name);  
+		Cookie cookie = new Cookie("username", lastName);  
 		cookie.setMaxAge(24*60*60);
 		response.addCookie(cookie);
 		PrintWriter out = response.getWriter();
-		out.println("<html>\n" +"<head><title>"+ title + "</title></head>\n" + " </body>Hello "+name+"</body></html>"); 
+		out.println("<html>\n" +"<head><title>"+ title + "</title></head>\n" + " </body>Hello "+firstName+" "+lastName+"</body></html>"); 
 		/*RequestDispatcher view = request.getRequestDispatcher(“cookieresult.jsp”);
 		view.forward(request, response); */
 		}
